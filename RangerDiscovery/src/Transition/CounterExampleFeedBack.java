177016b913400e28983e23c9f611443aa4ec51c5
sohah <husseinsoh@gmail.com>
2019-03-07 21:17:35 -0600

    adding counterExample changes

--- RangerDiscovery/src/Transition/CounterExampleFeedBack.java
+++ RangerDiscovery/src/Transition/CounterExampleFeedBack.java
@@ -4,6 +4,8 @@ import ast.Passes.RemoveHolesVisitor;
 import ast.Passes.ToAstPass;
 import ast.Passes.ToConstantHoleVisitor;
 import ast.def.*;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Solver;
 import ref.Pair;
 
 import java.util.HashMap;
@@ -14,14 +16,24 @@ import static Transition.TransitionT.transitionT;
 
 public class CounterExampleFeedBack {
 
+    public HashMap<String, String> cfg = new HashMap<String, String>();
 
+    Solver solver;
+    Context ctx;
+
+    public CounterExampleFeedBack(){
+        ctx = new Context(cfg);
+        cfg.put("proof", "true");
+
+
+    }
     public static void executeConstantCEFL(String expfileName) throws DiscoveryException {
         Pair<LinkedHashMap<String, Var>, Ast> contextAndBody = ToAstPass.execute(expfileName);
 
         transitionT.tContext = contextAndBody.getFirst();
         transitionT.tBody = (Exp) contextAndBody.getSecond();
 
-        public HashMap<Hole, Ast> instantiatedHoles = new HashMap<>();
+        HashMap<Hole, Ast> instantiatedHoles;
 
         boolean sat = true;
         boolean done = false;
@@ -32,12 +44,12 @@ public class CounterExampleFeedBack {
             transitionT.tHoleContext = contextAndBody.getFirst();
             transitionT.tHoleBody = (Exp) contextAndBody.getSecond();
 
-            checkSat();
-            instantiatedHoles = getModel();
+            checkTholeSat();
+            instantiatedHoles = getModelForHoles();
             transitionT.tPrimeBody = (Exp) RemoveHolesVisitor.execute(instantiatedHoles, transitionT.tHoleBody);
+            sat = checkTprimeSat();
 
-
-
+            done = true; // we want to stop after one  time change of constant.
         }
 
         if(sat)
@@ -45,5 +57,16 @@ public class CounterExampleFeedBack {
         else
             System.out.println("Successful repair was completed, repaired contract is:\n" + transitionT.tPrimeToString());
 
+    }
+
+    private static HashMap<Hole,Ast> getModelForHoles() {
+
+    }
+
+    private static boolean checkTprimeSat() {
+        return false;
+    }
+
+    private static void checkTholeSat() {
     }
 }
