508e43590d2e99b383a089232f3c5b0ca9dd2070
sohah <husseinsoh@gmail.com>
2019-03-08 05:22:24 -0600

    adding main invocation for the CEFLP

--- RangerDiscovery/src/Main.java
+++ RangerDiscovery/src/Main.java
@@ -1,24 +1,22 @@
 import Transition.CounterExampleFeedBack;
 import Transition.TransitionT;
 import ast.Passes.ToAstPass;
+import ast.def.DiscoveryException;
 
 public class Main {
     public static TransitionT transitionT = new TransitionT();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DiscoveryException {
 
         String folderName;
         folderName = "../RunPadModel/ImaginaryPad/";
         String disfileName = folderName + "UnbeliveablySimplePad.runPadSteps(IZZZZ)V#21_0.txt";
         String expfileName = folderName + "ImaginaryPad0.k-inductiontest.smt2";
 
-        CounterExampleFeedBack.execute(expfileName);
+        CounterExampleFeedBack.executeConstantCEFL(expfileName);
         /*ref.RangerQueryDiscovery rangerDiscovery = new ref.RangerQueryDiscovery();
         rangerDiscovery.discoverContract(disfileName, expfileName);
 
         ref.JavaExample.main(null);*/
-
-
     }
-
 }
