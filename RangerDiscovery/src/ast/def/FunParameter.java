acf726bb0952ceb24d16ec8ca02a74d0cc332617
sohah <husseinsoh@gmail.com>
2019-04-15 15:41:24 -0500

    adding step for T

--- RangerDiscovery/src/ast/def/FunParameter.java
+++ RangerDiscovery/src/ast/def/FunParameter.java
@@ -1,14 +1,34 @@
 package ast.def;
 
 
-public class FunParameter {
+import ast.visitors.AstVisitor;
+
+public class FunParameter extends Exp {
 
     Var parameter;
 
+    public Var getParameter() {
+        return parameter;
+    }
+
+    public ParameterType getParameterType() {
+        return parameterType;
+    }
+
     ParameterType parameterType;
 
-    public FunParameter(Var parameter, ParameterType parameterType){
+    public FunParameter(Var parameter, ParameterType parameterType) {
         this.parameter = parameter;
         this.parameterType = parameterType;
+    }
+
+
+    public FunParameter clone() {
+        return new FunParameter(this.parameter, this.parameterType);
+    }
+
+    @Override
+    public <T> T accept(AstVisitor<T> visitor) throws DiscoveryException {
+        return null;
     }
 }
--- RangerDiscovery/src/jContract/Step.java
+++ RangerDiscovery/src/jContract/Step.java
@@ -24,31 +24,30 @@ public class Step {
 
     WhichStep whichStep;
 
-    public Step(int k, WhichStep whichStep, Transition someT) {
-        this.k = k;
+    public Step(WhichStep whichStep, Transition someT) {
         this.whichStep = whichStep;
         this.someT = someT;
     }
 
     /**
      * creating of step, must be in the order of the parameters definition in each transition.
-     * @param someT
+     *
      * @param k
      * @return
      */
-    public StepOutput makeStep(Transition someT, ArrayList<Var> input, int k) {
-        Pair<ArrayList<Exp>, ArrayList<Var>> stepParamAndVar = someT.makeStepParameters(k);
+    public StepOutput makeStep(ArrayList<Var> input, int k) {
+        Pair<ArrayList<Exp>, ArrayList<Var>> stepParamAndVar = null;
         switch (someT.whichTransition) {
             case T:
             case HOLE:
-                stepParamAndVar = makeTstepParameters(someT, k);
+                stepParamAndVar = makeTstepParameters((T) someT, k);
                 break;
             case R:
-                stepParamAndVar = makeRstepParameters(someT, k);
+                stepParamAndVar = makeRstepParameters((R) someT, k);
                 break;
         }
 
-        assert(stepParamAndVar != null);
+        assert (stepParamAndVar != null);
         Assertion stepAssertion = new Assertion(new FunCall(someT, stepParamAndVar.getFirst()));
         ArrayList<Var> outputVars = stepParamAndVar.getSecond();
         StepOutput stepOutput = new StepOutput(stepAssertion, outputVars);
@@ -59,18 +58,166 @@ public class Step {
      * the purpose of this method is to create new vars with the right indexes for the required k.
      * it returns a pair of the Exp and Output Vars that were created resulting from the step.
      */
-    private Pair<ArrayList<Exp>,ArrayList<Var>> makeRstepParameters(Transition someT, int k) {
+    private Pair<ArrayList<Exp>, ArrayList<Var>> makeRstepParameters(R r, int k) {
+        ArrayList<FunParameter> originalParameters = r.parameters;
+
+        ArrayList<Exp> instantiationBinding = new ArrayList<>();
+        ArrayList<Var> outputVars = new ArrayList<>();
+
+
+        if (whichStep == WhichStep.BMC)
+            instantiationBinding.add(BoolConst.TRUE);
+        else {
+            Var firstVar = originalParameters.get(0).getParameter();
+            if (!firstVar.toString().equals("%init")) {
+                System.out.println("cannot have the first parameter in T anything but %init");
+                assert false;
+            }
+
+            instantiationBinding.add(firstVar.clone());
+        }
+
+        for (int i = 1; i < originalParameters.size(); i++) {
+            FunParameter parameter = originalParameters.get(i);
+            Var oldVar = parameter.getParameter();
+            Var newVar;
+            String oldVarNameNoScript = oldVar.name.substring(oldVar.name.length() - 1, oldVar.name.length() - 1);
+
+            switch (parameter.getParameterType()) {
+                case INPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    parameter = new FunParameter(newVar, ParameterType.INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case OUTPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.OUTPUT);
+                    instantiationBinding.add(parameter);
+                    outputVars.add(newVar);
+                    break;
+                case FREE_INPUT: // it uses the current k.
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.FREE_INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case DONT_CARE_INPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    parameter = new FunParameter(newVar, ParameterType.DONT_CARE_INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case DONT_CARE_OUTPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.DONT_CARE_OUTPUT);
+                    instantiationBinding.add(parameter);
+                    outputVars.add(newVar);
+                    break;
+                case INTERMEDIATE:
+                    System.out.println("This should not happen for T!");
+                    assert false;
+                    break;
+            }
+        }
+
+        return new Pair<>(instantiationBinding, outputVars);
     }
 
     /**
      * the purpose of this method is to create new vars with the right indexes for the required k.
-     * it returns a pair of the Exp and Output Vars that were created resulting from the step.
+     * it returns a pair of the Exp and Output Vars(Input Vars are assumed to have been created outside) that were
+     * created resulting from the step.
      */
-    private Pair<ArrayList<Exp>,ArrayList<Var>> makeTstepParameters(Transition someT, int k) {
+    private Pair<ArrayList<Exp>, ArrayList<Var>> makeTstepParameters(T t, int k) {
+        ArrayList<FunParameter> originalParameters = t.parameters;
 
+        ArrayList<Exp> instantiationBinding = new ArrayList<>();
+        ArrayList<Var> outputVars = new ArrayList<>();
+
+
+        if (whichStep == WhichStep.BMC)
+            instantiationBinding.add(BoolConst.TRUE);
+        else {
+            Var firstVar = originalParameters.get(0).getParameter();
+            if (!firstVar.toString().equals("%init")) {
+                System.out.println("cannot have the first parameter in T anything but %init");
+                assert false;
+            }
+
+            instantiationBinding.add(firstVar.clone());
+        }
+
+        for (int i = 1; i < originalParameters.size(); i++) {
+            FunParameter parameter = originalParameters.get(i);
+            Var oldVar = parameter.getParameter();
+            Var newVar;
+            String oldVarNameNoScript = oldVar.name.substring(oldVar.name.length() - 1, oldVar.name.length() - 1);
+
+            switch (parameter.getParameterType()) {
+                case INPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    parameter = new FunParameter(newVar, ParameterType.INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case OUTPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.OUTPUT);
+                    instantiationBinding.add(parameter);
+                    outputVars.add(newVar);
+                    break;
+                case FREE_INPUT: // it uses the current k.
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.FREE_INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case DONT_CARE_INPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k - 1)));
+                    parameter = new FunParameter(newVar, ParameterType.DONT_CARE_INPUT);
+                    instantiationBinding.add(parameter);
+                    break;
+                case DONT_CARE_OUTPUT:
+                    if (oldVar.type == Exp.VarType.INT)
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    else
+                        newVar = new IntVar((oldVarNameNoScript + (k)));
+                    parameter = new FunParameter(newVar, ParameterType.DONT_CARE_OUTPUT);
+                    instantiationBinding.add(parameter);
+                    outputVars.add(newVar);
+                    break;
+                case INTERMEDIATE:
+                    System.out.println("This should not happen for T!");
+                    assert false;
+                    break;
+            }
+        }
+
+        return new Pair<>(instantiationBinding, outputVars);
     }
-
-
 
 
 }
