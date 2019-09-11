71b7f808638caaf47e28f9007d5882bfe079d66c
sohah <husseinsoh@gmail.com>
2019-09-11 15:32:32 -0500

    adding sample program for ASM

---
+++ WorkSpace/src/GoToTransformation/GoToTransformer.java
@@ -0,0 +1,29 @@
+package GoToTransformation;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class GoToTransformer {
+
+
+    public static void execute() throws IOException {
+
+        Path pathRead = FileSystems.getDefault().getPath("resources", "VeritestingPerf.class");
+
+        Path pathWrite = FileSystems.getDefault().getPath("resources", "VeritestingPerfNew.class");
+
+
+        byte[] classByteRead = Files.readAllBytes(pathRead);
+
+        byte[] classByteWrite = MyTransformer.transform(classByteRead);
+
+        Files.write(pathWrite, classByteWrite);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        GoToTransformer.execute();
+    }
+}
